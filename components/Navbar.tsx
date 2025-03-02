a96853f32bfe4f93503a6dfa756dada4e1c57ccb
Arturo Parrales <arturo.ps14@gmail.com>
2025-03-02 15:59:09 -0500

    First commit that shows already navbar in initial nextjs app

---
+++ components/Navbar.tsx
@@ -0,0 +1,38 @@
+'use client';
+import Link from 'next/link';
+import { ModeToggle } from './theme-toggle';
+
+export const Navbar = () => {
+  const isSignedIn = true;
+
+  return (
+    <nav className="bg-background border-b px-12">
+      <div className="max-w-7xl mx-auto flex justify-between items-center h-16 py-2 text-lg">
+        <Link href="/" className="p-2 font-medium">
+          Saas
+        </Link>
+        <div className="space-x-2">
+          {isSignedIn ? (
+            <>
+              <Link href="/dashboard" className="p-2 font-medium">
+                Product
+              </Link>
+              <Link href="/dashboard/profile" className="p-2 font-medium">
+                Profile
+              </Link>
+            </>
+          ) : (
+            <></>
+          )}
+          <Link href="/pricing" className="p-2 font-medium">
+            Pricing
+          </Link>
+        </div>
+        <div className="space-x-4 flex items-center">
+          <ModeToggle />
+          {isSignedIn ? <a>Sign Out</a> : <a>Sign Out</a>}
+        </div>
+      </div>
+    </nav>
+  );
+};
