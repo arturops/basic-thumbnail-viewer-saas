91cefdee26bb5715f1bf013931f5447ae15c1995
Arturo Parrales <arturo.ps14@gmail.com>
2025-03-29 19:51:47 -0400

    Added logic in dashboard to identify active users and show respetive page

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,31 @@
 import { Button } from '@/components/ui/button';
 import Link from 'next/link';
+import prisma from '@/lib/db';
+import { auth } from '@clerk/nextjs/server';
+import { redirect } from 'next/navigation';
+import ThumbnailPreviewer from './_components/thumbnail-previewer';
 
 export default async function Dashboard() {
+  const { userId } = await auth();
+
+  if (!userId) {
+    return redirect('/sign-in');
+  }
+
+  const subscription = await prisma.subscription.findUnique({
+    where: { userId: userId },
+    select: { status: true },
+  });
+
+  if (subscription?.status === 'active') {
+    return (
+      <div className="flex flex-col items-center justify-center p-8 rounded-lg shadow-md">
+        <p className="text-xl  mb-6">You are subcribed!</p>
+        <ThumbnailPreviewer channelNameSaved="test" />
+      </div>
+    );
+  }
+
   return (
     <div className="flex flex-col items-center justify-center p-8 rounded-lg shadow-md">
       <p className="text-xl  mb-6">You are not subcribed</p>
